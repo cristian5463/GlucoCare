92f8764cd3c6a5cff97f5bc54f06396455248538
peixoto98 <85299686+peixoto98@users.noreply.github.com>
2024-04-25 12:05:54 -0300

    Update ConfigDTO.cs

--- GlucoCare.Application/DTOs/ConfigDTO.cs
+++ GlucoCare.Application/DTOs/ConfigDTO.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
-namespace GlucoCare.Application.DTOs
+namespace GlucoCare.Application.DTOs;
+public class ConfigDTO
 {
-    public class ConfigDTO
-    {
-        public bool ApplyInsulinSnack { get; set; }
-        public bool UseCarbsCalc { get; set; }
-    }
+    public bool ApplyInsulinSnack { get; set; }
+    public bool UseCarbsCalc { get; set; }
 }
