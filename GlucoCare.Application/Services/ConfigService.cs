f0e61cf27c583b2c75ffd165a9241868bb03ded3
peixoto98 <85299686+peixoto98@users.noreply.github.com>
2024-04-25 12:13:19 -0300

    Update ConfigService.cs

--- GlucoCare.Application/Services/ConfigService.cs
+++ GlucoCare.Application/Services/ConfigService.cs
@@ -11,22 +11,23 @@ using System.Threading.Tasks;
 namespace GlucoCare.Application.Services;
 public class ConfigService : IConfigService
 {
-    private readonly IConfigRepository _configRepository;
-    private readonly IMapper _mapper;
+    private IConfigRepository _configRepository;
+    private IMapper _mapper;
 
     public ConfigService(IMapper mapper, IConfigRepository configRepository)
     {
-        _configRepository = configRepository ?? throw new ArgumentNullException(nameof(configRepository));
-        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _configRepository = configRepository ?? 
+            throw new ArgumentNullException(nameof(configRepository));
+        _mapper = mapper;
     }
 
-    public async Task Add(ConfigDTO configDTO)
+    public Task Add(ConfigDTO configDTO)
     {
         var configEntity = _mapper.Map<ConfigEntity>(configDTO);
-        await _configRepository.CreateAsync(configEntity);
+        return _configRepository.CreateAsync(configEntity);
     }
 
-    public async Task<ConfigDTO> GetById(int id)
+    public async Task<ConfigDTO> GetById(int? id)
     {
         var configEntity = await _configRepository.GetByIdAsync(id);
         return _mapper.Map<ConfigDTO>(configEntity);
@@ -38,46 +39,16 @@ public class ConfigService : IConfigService
         return _mapper.Map<IEnumerable<ConfigDTO>>(configEntities);
     }
 
-    public async Task Remove(int id)
+    public async Task Remove(int? id)
     {
-        var configEntity = await _configRepository.GetByIdAsync(id);
-        if (configEntity != null)
-            await _configRepository.RemoveAsync(configEntity);
+        var configEntity = await _configRepository.GetByIdAsync(id).Result;
+        await _configRepository.RemoveAsync(configEntity);
+        
     }
 
     public async Task Update(ConfigDTO configDTO)
     {
         var configEntity = _mapper.Map<ConfigEntity>(configDTO);
         await _configRepository.UpdateAsync(configEntity);
-    }
-
-    // Implementação dos métodos da interface IConfigService
-    public async Task UpdateConfiguration(int id, ConfigDTO configDTO)
-    {
-        var configEntity = await _configRepository.GetByIdAsync(id);
-        if (configEntity != null)
-        {
-            _mapper.Map(configDTO, configEntity);
-            await _configRepository.UpdateAsync(configEntity);
-        }
-    }
-
-    public async Task<ConfigDTO> GetConfiguration(int id)
-    {
-        var configEntity = await _configRepository.GetByIdAsync(id);
-        return _mapper.Map<ConfigDTO>(configEntity);
-    }
-
-    public async Task DeleteConfiguration(int id)
-    {
-        var configEntity = await _configRepository.GetByIdAsync(id);
-        if (configEntity != null)
-            await _configRepository.RemoveAsync(configEntity);
-    }
-
-    public async Task CreateConfiguration(ConfigDTO configDTO)
-    {
-        var configEntity = _mapper.Map<ConfigEntity>(configDTO);
-        await _configRepository.CreateAsync(configEntity);
     }
 }
