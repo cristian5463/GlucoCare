d43f23b2480fbba1657e3df35e19485b89a4a6e9
peixoto98 <85299686+peixoto98@users.noreply.github.com>
2024-04-25 11:48:09 -0300

    Update ConfigEntity.cs

--- GlucoCara.Domain/Entities/ConfigEntity.cs
+++ GlucoCara.Domain/Entities/ConfigEntity.cs
@@ -4,12 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GlucoCare.Domain.Entities
 {
-    public class ConfigurationEntity
+    public class ConfigEntity
     {
+        [Key] 
         private bool _applyInsulinSnack;
         private bool _useCarbsCalc;
 
-        public ConfigurationEntity(bool applyInsulinSnack, bool useCarbsCalc)
+        public ConfigEntity(bool applyInsulinSnack, bool useCarbsCalc)
         {
             _applyInsulinSnack = applyInsulinSnack;
             _useCarbsCalc = useCarbsCalc;
