e5a271b6d01cc08a19c933d82bf7898bf68ede2c
peixoto98 <85299686+peixoto98@users.noreply.github.com>
2024-04-25 13:42:18 -0300

    Update ConfigRepository.cs

--- GlucoCare.Infrastructure/Repositories/ConfigRepository.cs
+++ GlucoCare.Infrastructure/Repositories/ConfigRepository.cs
@@ -1,55 +1,47 @@
+using GlucoCare.Domain.Entities;
 using GlucoCare.Domain.Interfaces;
-using GlucoCare.Infrastructure.Context;
-using GlucoCare.source.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; // Exemplo, ajuste de acordo com seu ORM
 
-namespace GlucoCare.Infrastructure.Repositories
+namespace GlucoCare.Infrastructure.Repositories;
+public class ConfigRepository : IConfigRepository
 {
-    public class ConfigRepository : IConfigRepository
+    private readonly DbContext _context;
+
+    public ConfigRepository(DbContext context)
     {
-        private readonly ApplicationDbContext _configContext;
+        _context = context;
+    }
 
-        public ConfigRepository(ApplicationDbContext context)
-        {
-            _configContext = context;
-        }
+    public async Task<IEnumerable<ConfigEntity>> GetConfigAsync(int idUser)
+    {
+        return await _context.Set<ConfigEntity>()
+            .Where(c => c.Id == idUser)
+            .ToListAsync();
+    }
 
-        public async Task<ConfigEntity> CreateAsync(ConfigEntity config)
-        {
-            _configContext.Add(config);
-            await _configContext.SaveChangesAsync();
-            return config;
-        }
+    public async Task<ConfigEntity> GetByIdAsync(int? id)
+    {
+        return await _context.Set<ConfigEntity>().FindAsync(id);
+    }
 
-        public async Task<ConfigEntity> RemoveAsync(ConfigEntity config)
-        {
-            _configContext.Remove(config);
-            await _configContext.SaveChangesAsync();
-            return config;
-        }
+    public async Task<ConfigEntity> CreateAsync(ConfigEntity config)
+    {
+        _context.Add(config);
+        await _context.SaveChangesAsync();
+        return config;
+    }
 
-        public async Task<ConfigEntity> GetByIdAsync(int? id)
-        {
-            return await _configContext.Config.SingleOrDefaultAsync(p => p.Id == id);
-        }
+    public async Task<ConfigEntity> UpdateAsync(ConfigEntity config)
+    {
+        _context.Entry(config).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return config;
+    }
 
-        public async Task<IEnumerable<ConfigEntity>> GetAllAsync()
-        {
-            return await _configContext.Config.ToListAsync();
-        }
-
-        public async Task<ConfigEntity> UpdateAsync(ConfigEntity config)
-        {
-            var existingConfig = await _configContext.FindAsync<ConfigEntity>(config.Id);
-
-            _configContext.Entry(existingConfig).CurrentValues.SetValues(config);
-            _configContext.Entry(existingConfig).Property(x => x.CreatedAt).IsModified = false;
-
-            await _configContext.SaveChangesAsync();
-
-            return existingConfig;
-        }
+    public async Task<ConfigEntity> RemoveAsync(ConfigEntity config)
+    {
+        _context.Remove(config);
+        await _context.SaveChangesAsync();
+        return config;
     }
 }
